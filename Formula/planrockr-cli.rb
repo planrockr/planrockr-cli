26ab74fe71e33b9beac24cb9e280c9a58bd4e6a4
Elton Minetto <eminetto@gmail.com>
2017-02-27 14:48:15 -0300

    Version update (#19)

--- Formula/planrockr-cli.rb
+++ Formula/planrockr-cli.rb
@@ -1,9 +1,8 @@
 class PlanrockrCli < Formula
   desc "planrockr-cli is a command line interface for the Planrockr API."
   homepage "https://github.com/planrockr/planrockr-cli"
-  url "https://github.com/planrockr/planrockr-cli/releases/download/v1.0/planrockr-cli-1.0.0-darwin-10.12-amd64.tar.gz"
-  sha256 "eb1b6aebb17f8804b9045cacbd1fddfb25b3b2ca96fb067c8c5f6d573b7d423d"
-
+  url "https://github.com/planrockr/planrockr-cli/releases/download/v1.0.1/planrockr-cli-1.0.1-darwin-10.12-amd64.tar.gz"
+  sha256 "90d9ebf22ea73ceab5a5cb9b82333f42e0fda813168b8a912311c5d10c926516"
 
   def install
     bin.install "planrockr-cli"
